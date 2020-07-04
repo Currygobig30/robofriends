1e739787d2e0f695f4161e6bf351666f4833126e
Chengyan <pcy00086@gmail.com>
2020-07-04 15:01:00 -0500

    Updates

---
+++ precache-manifest.9ce889b6a67c4ecc5688a456100a4f08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb78cafd26ab33cbcc1f7283f17534cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "b1826efff7ba4e4e831c",
+    "url": "/static/css/main.d79d52ad.chunk.css"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/static/js/2.f690e5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f690e5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1826efff7ba4e4e831c",
+    "url": "/static/js/main.2c81436b.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  }
+]);
